d99345bba2a67a5e133f6fd53fd4bce415724cdb
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-03-31 20:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,6 +12,17 @@ const getSum = () => {
     prices.forEach(price => {
         total += parseInt(price.innerText);
     });
+
+	 // Create a new row for total price
+    const table = document.querySelector("table");
+    const totalRow = document.createElement("tr");
+
+	// Create a single cell that spans both columns
+    const totalCell = document.createElement("td");
+    totalCell.setAttribute("colspan", "2");
+    totalCell.style.textAlign = "center";
+    totalCell.style.fontWeight = "bold";
+    totalCell.innerText = `Total Price: Rs ${total}`;
 		
   
 };
